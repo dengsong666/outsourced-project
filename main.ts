f6e736329fbb6db701b2ab2baee9f62e197955fb
dengsong666 <1487907946@qq.com>
2024-05-20 15:13:41 +0800

    🎉 init:

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 export { }
 const audioPlayer = new Audio("https://ds-blog.s3.bitiful.net/倒带.mp3")
-console.log(navigator.mediaDevices.getDisplayMedia)
-navigator.mediaDevices.getDisplayMedia({
+console.log(navigator.mediaDevices.getUserMedia)
+navigator.mediaDevices.getUserMedia({
   video: {
     width: { ideal: 1920, max: 1920 },
     height: { ideal: 1080, max: 1080 }
